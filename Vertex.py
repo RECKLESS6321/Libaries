3f5a79331967b8d85c272654b50a4a1153db865e
RECKLESS <66432554+RECKLESS6321@users.noreply.github.com>
2020-07-11 23:56:01 +0530

    Update Vertex.py

--- Vertex.py
+++ Vertex.py
@@ -1,3 +1,7 @@
+#Uses a dictionary as an adjacency list to store connected vertices.
+#Connected vertex names are keys and the edge weights are values.
+#Has methods to add edges and return a list of connected vertices.
+
 class Vertex:
   def __init__(self, value):
     self.value = value
