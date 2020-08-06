5534b253f6eb90fef4a26e1acda1d371a08d227b
RECKLESS <66432554+RECKLESS6321@users.noreply.github.com>
2020-08-06 19:43:13 +0530

    Create Bubble sort.py

---
+++ Bubble sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(arr):
+  for i in range(len(arr)):
+    # iterate through unplaced elements
+    for idx in range(len(arr) - i - 1):
+      if arr[idx] > arr[idx + 1]:
+        # replacement for swap function
+        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
