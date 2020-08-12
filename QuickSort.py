4143116a9585dd494203077b1f073092f49d8ff6
RECKLESS <66432554+RECKLESS6321@users.noreply.github.com>
2020-08-12 20:23:47 +0530

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -1,28 +1,31 @@
-from random import randrange
+from random import randrange, shuffle
 
 def quicksort(list, start, end):
+  # this portion of list has been sorted
   if start >= end:
-    return list
-
-  pivot_idx = randrange(start, end)
+    return
+  
+  # select random element to be pivot
+  pivot_idx = randrange(start, end + 1)
   pivot_element = list[pivot_idx]
+  
+  # swap random element with last element in sub-lists
   list[end], list[pivot_idx] = list[pivot_idx], list[end]
 
-  # Create the lesser_than_pointer
+  # tracks all elements which should be to left (lesser than) pivot
+  less_than_pointer = start
   
-  # Start a for loop, use 'idx' as the variable
-    # Check if the value at idx is less than the pivot
-      # If so: 
-        # 1) swap lesser_than_pointer and idx values
-        # 2) increment lesser_than_pointer
+  for i in range(start, end):
+    # we found an element out of place
+    if list[i] < pivot_element:
+      # swap element to the right-most portion of lesser elements
+      
+      list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
+      # tally that we have one more lesser element
+      less_than_pointer += 1
+  # move pivot element to the right-most portion of lesser elements
+  list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
   
-  # After the loop is finished...
-  # swap pivot with value at lesser_than_pointer
-  print(list[start])
-  start += 1
-  return quicksort(list, start, end)
-
-my_list = [42, 103, 22]
-print("BEFORE: ", my_list)
-sorted_list = quicksort(my_list, 0, len(my_list) - 1)
-print("AFTER: ", sorted_list)
+  # recursively sort left and right sub-lists
+  quicksort(list, start, less_than_pointer - 1)
+  quicksort(list, less_than_pointer + 1, end)
